9d5fc76f86afccaa2b3bc00d919e463d72d86df6
Sean Desktop <seankraft@hotmail.com>
2015-10-04 22:38:12 -0700

    adding basic button functions

--- app/src/main/java/com/example/seankraft/constructioncalc/MainActivity.java
+++ app/src/main/java/com/example/seankraft/constructioncalc/MainActivity.java
@@ -1,17 +1,76 @@
 package com.example.seankraft.constructioncalc;
 
-import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    ArrayList<String> operationList = new ArrayList<>();
+    String buttonString = "";
+    String numInputString = "";
+
+
+
+
+    // for rounding to a specific fraction resolution:
+    //Math.round(myFloat*16) / 16f
+
+    // function to receive number button clicks
+    public void clickNumber(View view) {
+        TextView outputText = (TextView) findViewById(R.id.outputText);
+        Button numberButton = (Button) view;
+        buttonString = numberButton.getText().toString();
+        // add the number to the output string for the view
+        numInputString = numInputString + buttonString;
+        outputText.setText(numInputString);
+    }
+
+    // function to receive operation button clicks
+    public void clickOperation(View view) {
+        // add the current concatenated number to operation list
+        operationList.add(numInputString);
+        // add the clicked operator to the operation list
+        Button numberButton = (Button) view;
+        buttonString = numberButton.getText().toString();
+        operationList.add(buttonString);
+        // update the DEBUG window
+        TextView debugText = (TextView) findViewById(R.id.debug);
+        debugText.setText(operationList.toString());
+        // clear the numInputString for the next number entry
+        numInputString = "";
+    }
+
+    public void clickCalculate(View view) {
+        // add the current concatenated number to operation list
+        operationList.add(numInputString);
+        // update the DEBUG window
+        TextView debugText = (TextView) findViewById(R.id.debug);
+        debugText.setText(operationList.toString());
+    }
+
+    public void clear(View view) {
+        TextView outputText = (TextView) findViewById(R.id.outputText);
+        TextView debugText = (TextView) findViewById(R.id.debug);
+        outputText.setText("0");
+        debugText.setText("");
+        // clear variables
+        operationList = new ArrayList<>();
+        numInputString = "";
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
